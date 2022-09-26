daf734970014f1a2f78343cb3eb73cf689d2d214
nicomiguelino <nicomiguelino2014@gmail.com>
2022-09-25 18:02:55 -0700

    Add initial changes on using custom error responses.
    - Remove debug statements later.

--- graphql_api/schema/__init__.py
+++ graphql_api/schema/__init__.py
@@ -16,10 +16,31 @@ from .tweet import TweetType
 class Query(ObjectType):
     all_tweets = List(TweetType)
 
-    @login_required
+    # nico start - old code
+    # todo nico: remove this block if not needed
+    # @login_required
+    # def resolve_all_tweets(root, info):
+    #     user = info.context.user
+    #     return Tweet.objects.all()
+    # nico end
+
+    # nico start - new code
     def resolve_all_tweets(root, info):
         user = info.context.user
-        return Tweet.objects.all()
+        if not user.is_authenticated:
+            raise Exception('Lorem ipsum dolor sit amet.')
+    # nico end
+
+
+# nico start - new code
+class CustomObtainJSONWebToken(ObtainJSONWebToken):
+    @classmethod
+    def mutate(cls, *args, **kwargs):
+        try:
+            return super().mutate(*args, **kwargs)
+        except:
+            raise Exception
+# nico end
 
 
 class Mutation(ObjectType):
